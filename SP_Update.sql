2d9ea981af3a6aa32855452c440ea85cff57266d
juanjosecorella <juanjosecorella2004@gmail.com>
2024-06-10 22:46:29 -0600

    cambiar formato de codigozapato a `{tipo}-{color}-{talla}`
    añadir update sp's para color y tipo

--- SP_Update.sql
+++ SP_Update.sql
@@ -1,3 +1,6 @@
+-- Proveedor
+
+
 USE KOALASA
 GO
 CREATE PROCEDURE SP_EDITARTELEFONO_PROVEEDOR(
@@ -154,4 +157,131 @@ GO
 Execute SP_REACTIVAR_PROVEEDOR 'P-0687'
 select * from Proveedor
 
+-- Tipo
 
+USE KOALASA
+GO
+
+CREATE PROCEDURE SP_EDITAR_NOMBRE_TIPO @CodigoTipo INT, @NombreTipo VARCHAR(10)
+AS
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM Tipo WHERE CodigoTipo = @CodigoTipo)
+        BEGIN
+            PRINT 'El tipo no existe'
+            RETURN
+        END
+    
+    IF @NombreTipo = ''
+        BEGIN
+            PRINT 'No se permite un tipo vacio'
+            RETURN
+        END
+
+    UPDATE Tipo
+    SET NombreTipo = @NombreTipo
+    WHERE CodigoTipo = @CodigoTipo;
+
+    PRINT 'Se cambio el nombre del tipo exitosamente'
+END
+GO
+
+USE KOALASA
+GO
+
+CREATE PROCEDURE SP_EDITAR_NOMBRE_TIPO @CodigoTipo INT, @NombreTipo VARCHAR(10)
+AS
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM Tipo WHERE CodigoTipo = @CodigoTipo)
+        BEGIN
+            PRINT 'El tipo no existe'
+            RETURN
+        END
+    
+    IF @NombreTipo = ''
+        BEGIN
+            PRINT 'No se permite un tipo vacio'
+            RETURN
+        END
+
+    UPDATE Tipo
+    SET NombreTipo = @NombreTipo
+    WHERE CodigoTipo = @CodigoTipo;
+
+END
+GO
+
+-- 
+
+USE KOALASA
+GO
+
+CREATE PROCEDURE SP_EDITAR_PROVEEDOR_TIPO @CodigoTipo INT, @CodigoProveedor VARCHAR(10)
+AS
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM Tipo WHERE CodigoTipo = @CodigoTipo)
+        BEGIN
+            PRINT 'El tipo no existe'
+            RETURN
+        END
+    
+    IF @CodigoProveedor = ''
+        BEGIN
+            PRINT 'No se permite un proveedor vacio'
+            RETURN
+        END
+
+    UPDATE Tipo
+    SET CodigoProveedor = @CodigoProveedor
+    WHERE CodigoTipo = @CodigoTipo;
+
+END
+GO
+
+-- Color
+
+USE KOALASA
+GO
+
+CREATE PROCEDURE SP_EDITAR_NOMBRE_COLOR @CodigoColor INT, @NombreColor VARCHAR(10)
+AS
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM Color WHERE CodigoColor = @CodigoColor)
+        BEGIN
+            PRINT 'El color no existe'
+            RETURN
+        END
+    
+    IF @CodigoColor = ''
+        BEGIN
+            PRINT 'No se permite un nombre vacio'
+            RETURN
+        END
+
+    UPDATE Color
+    SET NombreColor = @NombreColor
+    WHERE CodigoColor = @CodigoColor;
+
+END
+GO
+
+-- Zapato
+
+
+USE KOALASA
+GO
+
+CREATE PROCEDURE SP_EDITAR_TIPO_ZAPATO @CodigoZapato varchar(10), @CodTipo INT
+AS
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM Zapato WHERE CodigoZapato = @CodigoZapato)
+        BEGIN
+            PRINT 'El zapato no existe'
+            RETURN
+        END
+    
+    UPDATE Zapato
+    SET CodTipo = @CodTipo
+    WHERE CodigoZapato = @CodigoZapato;
+
+END
+GO
